a47721fcc371bc18bd787859fec5a400770f7e6a
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2021-09-01 19:32:32 +0700

    Initial commit

---
+++ T2004_Pracical/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace T2004_Pracical.Models
+{
+    public partial class Student
+    {
+        public string StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ T2004_Pracical/Models/T2004E_Practical3Context.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace T2004_Pracical.Models
+{
+    public partial class T2004E_Practical3Context : DbContext
+    {
+        public T2004E_Practical3Context()
+        {
+        }
+
+        public T2004E_Practical3Context(DbContextOptions<T2004E_Practical3Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-LSV7E0T\\HIEU;Initial Catalog=T2004E_Practical3;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.StudentId)
+                    .HasMaxLength(10)
+                    .HasColumnName("StudentID")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

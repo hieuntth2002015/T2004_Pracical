89558b9bf875aa1d5ce19f031f33dac38a818e48
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2021-09-01 19:33:50 +0700

    update
    DB

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿-- =========================================
+-- Create FileTable template
+-- =========================================
+USE T2004E_Practical3
+GO
+
+INSERT INTO Students VALUES 
+(100, 'Nguyễn', 'Văn A', 0912345678, 'nguyenvana@gmail.com'),
+(101, 'Nguyễn', 'Thị B', 0987654321, 'nguyenthib@gmail.com'),
+(102, 'Hoàng', 'Văn C', 0988545234, 'hoangvanc@gmail.com'),
+(103, 'Lưu', 'Ngọc H', 0123456234, 'luungoch@gmail.com'),
+(104, 'Ma', 'Thi T', 0789654352, 'mathit@gmail.com');
+GO
